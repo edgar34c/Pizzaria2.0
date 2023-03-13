b70fae156b6c1c0b1a6988e023406902057bde46
edgar34c <edgararaujocorrea@gmail.com>
2023-03-13 13:50:05 -0300

    identando código e adicionando grid

--- View/index.php
+++ View/index.php
@@ -4,65 +4,77 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Pagina Inicial</title>
+    <title>Página Inicial</title>
     <link rel="stylesheet" href="css/index.css">
 </head>
 <body>
    
     <header>
+        <!--Menu superior!-->
         <ol>
-            <li><img src="image/casa.png" id="icon_casa"></li>
-            <li><a href="<?php echo URL;?>home">Página inicial</a></li>
-            <li><img src="image/telefone.png" id="icon_tel"></li>
-            <li><a href="<?php echo URL;?>fale">Fale Conosco</a></li>
-            <li><img src="image/sobre.png" id="icon_sobre"></li>
-            <li><a href="#">Sobre nós</a></li>
-            <li><img src="image/login.png" id="icon_login"></li>
-            <li><a href="<?php echo URL;?>abrirlogar">Entrar</a></li>
+            <div class="center">
+                <li><img src="image/casa.png" id="icon_casa"></li>
+                <li><a href="<?php echo URL;?>home">Página inicial</a></li>
+                <li><img src="image/telefone.png" id="icon_tel"></li>
+                <li><a href="<?php echo URL;?>fale">Fale Conosco</a></li>
+                <li><img src="image/login.png" id="icon_login"></li>
+                <li><a href="<?php echo URL;?>abrirlogar">Entrar</a></li>
+            </div>
         </ol>
+    </header>
 
-        <div class="slider">
+    <div class="slider">
 
-            <div class="slides">
-             <!--Radio Buttons-->
-             <input type="radio" name="radio-btn" id="radio1">
-             <input type="radio" name="radio-btn" id="radio2">
-             <input type="radio" name="radio-btn" id="radio3">
-             <input type="radio" name="radio-btn" id="radio4">
+        <div class="slides">
+                <!--Radio Buttons-->
+                <input type="radio" name="radio-btn" id="radio1">
+                <input type="radio" name="radio-btn" id="radio2">
+                <input type="radio" name="radio-btn" id="radio3">
+                <input type="radio" name="radio-btn" id="radio4">
     
-              <!--Slide images-->
-              <div class="slide first">
-                <img src="https://img.freepik.com/fotos-gratis/pizza-pizza-cheia-de-tomates-salame-e-azeitonas_140725-1200.jpg?w=2000" alt="imagem 1">
-             </div>
-              <div class="slide">
-                <img src="https://st3.depositphotos.com/1868027/15009/i/450/depositphotos_150093080-stock-photo-italian-pizza-tomatoes-salami-black.jpg" alt="imagem 2">
-               </div>
-               <div class="slide">
-                <img src="https://s.yimg.com/ny/api/res/1.2/SEMowe8X4DsmdX7uum7gYg--/YXBwaWQ9aGlnaGxhbmRlcjt3PTY0MDtoPTM4Mw--/https://s.yimg.com/os/creatr-uploaded-images/2021-10/c5fd29a0-2b9f-11ec-b3ff-30a01d58d24a" alt="imagem 3 ">
-               </div>
-    
-               <div class="slide">
-                <img src="https://viagemegastronomia.cnnbrasil.com.br/wp-content/uploads/sites/5/2022/04/La-Braciera_Pizza-Italiana_Neuton-Araujo.jpg" alt="imagem 4">
-               </div>
-    
-               <!--navogation-->
-                <div class="navigation-auto">
-                <div class="auto-btn1"></div>
-                <div class="auto-btn2"></div>
-                <div class="auto-btn3"></div>
-                <div class="auto-btn4"></div>
+                <!--Slide images-->
+                <div class="slide first">
+                    <img src="image/pizza3.jpg" alt="imagem 1">
+                </div>
+                <div class="slide">
+                    <img src="image/pizza2.png" alt="imagem 2">
+                </div>
+                <div class="slide">
+                    <img src="image/pizza1.jpeg" alt="imagem 3 ">
                 </div>
     
-            </div>
+               <div class="slide">
+                <img src="image/pizza5.png" alt="imagem 4">
+               </div>
     
-             <div class="manual-navigation">
-              <label for="radio1" class="manual-btn"></label>
-             <label for="radio2" class="manual-btn"></label>
-              <label for="radio3" class="manual-btn"></label>
-              <label for="radio4" class="manual-btn"></label>
-             </div>
+                <!--navogation-->
+                <div class="navigation-auto">
+                    <div class="auto-btn1"></div>
+                    <div class="auto-btn2"></div>
+                    <div class="auto-btn3"></div>
+                    <div class="auto-btn4"></div>
+                </div>
+    
         </div>
-    </header>
+    
+            <div class="manual-navigation">
+                <label for="radio1" class="manual-btn"></label>
+                <label for="radio2" class="manual-btn"></label>
+                <label for="radio3" class="manual-btn"></label>
+                <label for="radio4" class="manual-btn"></label>
+            </div>
+    </div>
+
+    <div class="grid-pizza">
+        <a href="<?php echo URL;?>abrirlogar"><figure><img src="image/pizza_mussarela.png" alt="Pizza mussarela" class="img_pizzas" ><figcaption>Mussarela</figcaption></figure></a>
+        <a href="<?php echo URL;?>abrirlogar"><figure><img src="image/pizza_calabresa.png" alt="Pizza calabresa" class="img_pizzas"><figcaption>Calabresa</figcaption></figure></a>
+        <a href="<?php echo URL;?>abrirlogar"><figure><img src="image/pizza_portuguesa.png" alt="Pizza portuguesa" class="img_pizzas"><figcaption>Portuguesa</figcaption></figure></a>
+        <a href="<?php echo URL;?>abrirlogar"><figure><img src="image/pizza_marguerita.png" alt="Pizza marguerita" class="img_pizzas"><figcaption>Marguerita</figcaption></figure></a>
+        <a href="<?php echo URL;?>abrirlogar"><figure><img src="image/pizza_frango.png" alt="Frango com catupiry" class="img_pizzas"><figcaption>Frango com catupiry</figcaption></figure></a>
+        <a href="<?php echo URL;?>abrirlogar"><figure><img src="image/pizza_napolitana.png" alt="Napolitana" class="img_pizzas"><figcaption>Napolitana</figcaption></figure></a>
+    </div>
+
+
 </body>
 </html>
 
